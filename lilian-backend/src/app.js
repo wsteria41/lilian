61c0e05d10d30526c042e960ae80f47ab25f55e4
wsteria41 <bolor4821@gmail.com>
2026-08-15 14:45:35 +0800

    first commit

---
+++ lilian-backend/src/app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const cors = require('cors')
+const pool = require('./config/db')
+require('dotenv').config()
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.json({message: 'Lilian backend API running.'})
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
+
+//test db
+pool.query('SELECT NOW()', (err, res)=> {
+    if(err){
+        console.log('Connection lost',err)
+    }else{
+        console.log('Connected to db', res.rows[0])
+    }
+})
+
+module.exports = app
